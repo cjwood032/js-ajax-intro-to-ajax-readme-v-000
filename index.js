fc88e936f7284b91aab913580237f986163fe858
Christopher Wood <cjwood032@gmail.com>
2018-09-21 21:55:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-function showRepositories() {
-    //this is set to the XMLHttpRequest object that fired the event
-    console.log(this.responseText);
-  }
-   
-  function getRepositories() {
-    const req = new XMLHttpRequest();
-    req.addEventListener('load', showRepositories);
-    req.open('GET', 'https://api.github.com/users/octocat/repos');
-    req.send();
-  }
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
